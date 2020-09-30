87cbaad9fa2f3af41d7624046f2b6bf21a888502
“Mineliegoma” <garlandegoma@gmail.com>
2020-09-30 15:39:25 -0400

    comment working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", e => {
   url = "http://localhost:3000/api/v1/photos/"
   const photoDiv = document.querySelector('#dog-post')
+  const formAddPhoto = document.querySelector('.add-photo')
 
   const getPhotos = () => {
     fetch(url)
@@ -10,22 +11,15 @@ document.addEventListener("DOMContentLoaded", e => {
   //get all the photos and append it to the dom
   const renderPhotos = photos => {
     const photoDiv = document.querySelector('#dog-post')
-    //photoDiv.innerHTML = ''
     for (let photo of photos) {
-
       let newDiv = renderPhoto(photo)
       photoDiv.append(newDiv)
-
     }
-
-
   }
   //create an element
   // give that element a dataset attribute
   // create a innerHtml for the photos attributes
   //return the element
-
-
   const renderPhoto = photo => {
     const newDiv = document.createElement('div')
     newDiv.dataset.photoId = photo.id
@@ -47,14 +41,13 @@ document.addEventListener("DOMContentLoaded", e => {
 
 
   // click handler on photos using event delegations 
+  //////////////////////////////////////////////////
   const clickHandler = () => {
     document.addEventListener('click', e => {
       //console.log(e.target)
-      //event delegation
       if (e.target.className === 'like-btn') {
         const likeBtn = e.target
         const branch = likeBtn.previousElementSibling
-        //const ay = likeBtn.closest('div')
         const numberLikes = branch.previousElementSibling
         let newLikes = parseInt(numberLikes.textContent) + 1
         //const pressLikes = num + parseInt(1)
@@ -62,9 +55,7 @@ document.addEventListener("DOMContentLoaded", e => {
         numberLikes.textContent = newLikes + " "
           + "likes"
         let myLikes = numberLikes.textContent
-
         const id = likeBtn.dataset.photoId
-
         console.log(id)
 
         const options = {
@@ -95,23 +86,73 @@ document.addEventListener("DOMContentLoaded", e => {
           .then(photo => bttn.parentNode.remove())
 
       } else if (e.target.matches('.comment-button')) {
-        console.log(e.target)
+        //console.log(e.target)
         //first i delete the inner html of the photo container
         // second, i traversed the dom to reach to the div of the element that i clicked and insert it to a constant
         //third, i append that that constant to the photo container that was deleted.
 
-        //const bttn = e.target
+
         photoDiv.innerHTML = ''
         const btn = e.target
         //console.dir(btn)
         const wo = btn.parentElement
-        console.log(wo)
+        //console.log(btn)
         photoDiv.append(wo)
 
         //create a form for comments
-        // const formComment = document.querySelector(form)
-        // form.dataset.photoId2 = photo.id
-        // form.classList.add('')
+        // give it the dogs id
+        const formComment = document.createElement('form')
+        formComment.dataset.commentId = wo.dataset.photoId
+        formComment.classList.add('comment-form')
+        formComment.innerHTML = `
+        <label>name:</label>
+        <input type="text" name="name" value="" placeholder="Enter name">
+        <label>thoughts: </label>
+        <input type="text" name="thoughts" value="" placeholder="Enter comments">
+        <input type="submit" value="share thoughts" class="submit-comment">`
+        photoDiv.insertAdjacentElement('beforebegin', formComment)
+        // desactivate the comment button click and the add post click and deletebutton
+        btn.className = 'none'
+        formAddPhoto.innerHTML = ''
+        photoDiv.children[0].children[7].className = 'none'
+        //console.log(photoDiv.children[0].children[7].className)
+
+        //console.log(formComment)
+        /////////////////////////////////////////////////////
+        //lets grab the inputs from the form
+        // add a submit listener to the form
+        // const commentSubmitHandler = () => {
+        const forme = document.querySelector('.comment-form')
+        forme.addEventListener('submit', e => {
+          e.preventDefault()
+          const btn = e.target
+          const theName = forme.name.value
+          const comment = forme.thoughts.value
+          //console.log(theName, comment)
+          ////////////////////////////////////////////////////
+          //create a li
+
+          const ulEl = document.createElement('ul')
+          ulEl.dataset.ulId = btn.dataset.commentId
+          //console.log(ulEl)
+          const commentLi = document.createElement('li')
+          commentLi.textContent = theName + "\xa0\xa0\xa0\xa0\xa0" + "Comment:" + "   " + comment
+          ulEl.append(commentLi)
+          photoDiv.append(commentLi)
+          forme.reset()
+          //console.log(btn)
+          //"\xa0\xa0\xa0\xa0\xa0\xa0"
+
+
+
+        })
+
+        // need desactivate the comment button and the the create form
+
+
+
+
+
 
 
 
@@ -120,11 +161,23 @@ document.addEventListener("DOMContentLoaded", e => {
     })
   }
 
+  /////////////////////////////////////////////////////////
+  // const commentSubmitHandler = () => {
+  //   const formThougts = document.querySelector('.comment-form')
+  //   //formThougts.addEventListener('submit', e => {
+  //   //   e.preventDefault()
+  //   console.log(formThougts)
+  //   // })
+
+  // }
+
+  //////////////////////////////////////////////////////////////
   const submitHandler = () => {
-    document.addEventListener('submit', e => {
+    const form = document.querySelector('.add-photo')
+    form.addEventListener('submit', e => {
       e.preventDefault()
-      const form = document.querySelector('.add-photo')
-      //console.log(e.target)
+      //const form = document.querySelector('.add-photo')
+      //console.log('yay')
       const name = form.name.value
       const imageUrl = form.imageUrl.value
       //console.log(name, imageUrl)
@@ -161,6 +214,7 @@ document.addEventListener("DOMContentLoaded", e => {
   getPhotos()
   clickHandler()
   submitHandler()
+
 
 
 })
